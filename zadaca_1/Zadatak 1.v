46c667a2ce8793ec199f2bc4a117c472e9767a19
Kova <marin.kovac04@gmail.com>
2021-11-29 13:19:12 +0100

    Mala greskica ispravljena

--- zadaca_1/Zadatak 1.v
+++ zadaca_1/Zadatak 1.v
@@ -1,13 +1,25 @@
-Goal forall X Y, ~(X /\ Y) \/ (~X /\ Y) \/ (~X /\ ~Y) -> ~(X /\ Y).
+Goal forall X Y, ~(X /\ Y) \/ (~X /\ Y) \/ (~X /\ ~Y) <-> ~(X /\ Y).
 Proof.
-intros. destruct H.
-- exact H.
-- destruct H.
-+ unfold not. intros. apply H. destruct H0. exact H0.
-+ destruct H. unfold not. intros. destruct H1. apply H. exact H1.
+split.
+- intros. destruct H.
++ exact H.
++ destruct H.
+++ unfold not. intros. apply H. destruct H0. exact H0.
+++ destruct H. unfold not. intros. destruct H1. apply H. exact H1.
+- intros. left. exact H.
 Qed.
 
-Goal forall X Y Z, ~(~X /\ Y /\ ~Z) /\ ~(X /\ Y /\ Z) /\ (X /\ ~Y /\ ~Z) -> (X /\ ~Y /\ ~Z).
+Goal forall X Y Z, ~(~X /\ Y /\ ~Z) /\ ~(X /\ Y /\ Z) /\ (X /\ ~Y /\ ~Z) <-> (X /\ ~Y /\ ~Z).
 Proof.
-intros. destruct H. destruct H0. exact H1.
+split.
+- intros. destruct H. destruct H0. exact H1.
+- intros. split.
+-- destruct H. destruct H0. unfold not. intros. destruct H2. apply H2. exact H.
+-- destruct H. destruct H0. unfold not. split.
++ intros. destruct H2. destruct H3. apply H1. exact H4.
++ split.
+++ exact H.
+++ split.
++++ exact H0.
++++ exact H1.
 Qed.
